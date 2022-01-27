4d15145302309037886d6582f905c8b3b01ab7da
Sklyarov Mikhail <msklyarov@ozon.ru>
2022-01-27 18:18:22 +0300

    init

---
+++ src/components/Block.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "Block"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/String.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "String"
+}
+</script>
+
+<style scoped>
+
+</style>
